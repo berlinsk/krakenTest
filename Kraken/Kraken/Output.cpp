265aaa9d24d7796ff53c05d369bce2e2eceff1e5
berlinsky <berliniumyar@gmail.com>
2021-03-01 09:40:12 +0200

    Adding an Output

---
+++ Kraken/Kraken/Output.cpp
@@ -0,0 +1,8 @@
+//
+//  Output.cpp
+//  Kraken
+//
+//  Created by Берлинский Ярослав Владленович on 01.03.2021.
+//
+
+#include "Output.hpp"
---
+++ Kraken/Kraken/Output.hpp
@@ -0,0 +1,13 @@
+//
+//  Output.hpp
+//  Kraken
+//
+//  Created by Берлинский Ярослав Владленович on 01.03.2021.
+//
+
+#ifndef Output_hpp
+#define Output_hpp
+
+#include <stdio.h>
+
+#endif /* Output_hpp */
